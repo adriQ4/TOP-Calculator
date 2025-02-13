4c0cefc74ea6c214faf116bcfab758543dfa1f35
adriq4 <adri230992@gmail.com>
2025-02-13 10:08:14 +0100

    modify some the html, adding style

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hola mundo");
+// function operate(num1,op,num2){
+//     switch (op){
+//       case "+":
+//       console.log(num1 + num2);
+//       break;
+
+//       case "-":
+//       console.log(num1 - num2);
+//       break;
+
+//       case "*":
+//       console.log(num1 * num2);
+//       break;
+
+//       case "/":
+//       console.log(num1 / num2)
+//       break;
+
+//       default:
+//       console.log('Op need like +,-,*,/')
+
+//     }
+
+//   }
